ee2a955bb5432fa5ab28126529bd5ec81f84e049
Alvaro Molina <alvaromolina90@gmail.com>
2023-10-17 16:21:03 -0300

    Configuracion de Middlewares

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from "express";
-
+import cors from "cors";
+import morgan from "morgan";
+import "dotenv/config";
 // Configuracion inicial
 const app = express();
 
 app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), () => {
+  console.log("Estoy en el puerto " + app.get("port"));
+});
 // Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 // Crear las rutas
